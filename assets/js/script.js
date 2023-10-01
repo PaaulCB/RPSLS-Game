ebca87bfd8efdbe1389bc273c57bc7b27a5f427b
PaaulCB <>
2023-10-01 15:11:19 +0000

    Add structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function startGame(diff, mode) {
+
+}
+
+function cpuPick(diff) {
+
+}
+
+function pickWin(player) {
+
+}
+
+function pickLose(player) {
+
+}
+
+function checkWin(player, cpu) {
+
+}
+
+function updateWin() {
+
+}
+
+function updateLose() {
+
+}
+
+function showInstructions() {
+
+}
+
+function hideInstructions() {
+
+}
+
+function endGame() {
+
+}
