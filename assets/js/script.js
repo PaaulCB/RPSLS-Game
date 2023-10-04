37b5e92f539e8bfa8a25415af63d770a3d19f6b1
PaaulCB <>
2023-10-04 09:00:48 +0000

    Add code for cpuPick function and constant gamePicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     instr.previousElementSibling.addEventListener("click", showInstructions);
     instr.children[1].addEventListener("click", hideInstructions);
 });
+
 //Global var to check if its the first time that the user starts the game
 var firstTime = true;
+//Global constant to store the possible options
+const gamePicks = [
+    ["scissors", "icon 1"],
+    ["paper", "icon 2"],
+    ["rock", "icon 3"],
+    ["lizard", "icon 4"],
+    ["spoke", "icon 5"]];
 
 function startGame() {
     //Makes the game area visible
@@ -26,12 +34,13 @@ function startGame() {
         for (let option of options) {
             let pick = option.getAttribute("data-option");
             console.log(pick);
-            option.addEventListener("click", checkWin(pick, cpuPick(diff)));
+            option.addEventListener("click", checkWin(pick, cpuPick(diff, pick)));
         }
 
         firstTime = false;
     }
 }
+
 //**Gets the game difficulty when the player starts a game */
 function getDifficulty() {
     let difficulties = document.getElementsByName("difficulty");
@@ -41,6 +50,7 @@ function getDifficulty() {
         }
     }
 }
+
 //**Gets the game mode when the player starts a game */
 function getMode() {
     let modes = document.getElementsByName("mode");
@@ -50,9 +60,30 @@ function getMode() {
         }
     }
 }
-
-function cpuPick(diff) {
-
+//**Gets the cpu pick depending on the game difficulty */
+function cpuPick(diff, pick) {
+    let randomPick = gamePicks[Math.floor(Math.random() * 5)][0];
+    switch (diff) {
+        //If the difficulty is 1, 25% of the times will pick a lossing option
+        case 1:
+            if ((Math.random() * 100 + 1) <= 25) {
+                return pickLose(pick);
+            } else {
+                return randomPick;
+            }
+        //If the difficulty is 2 will pick a random option 
+        case 2:
+            return randomPick;
+        //If the difficulty is 3, 25% of the times will pick a winner option
+        case 3:
+            if ((Math.random() * 100 + 1) <= 25) {
+                return pickLose(pick);
+            } else {
+                return randomPick;
+            }
+        default:
+            alert("Error, invalid difficulty");
+    }
 }
 
 function pickWin(player) {
@@ -80,6 +111,7 @@ function updateLose() {
 function showInstructions() {
     document.getElementById("instructions").style.display = "block";
 }
+
 /**Hide the instruccions when the user clicks on the close button
  */
 function hideInstructions() {
